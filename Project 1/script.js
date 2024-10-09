0612639e4fc6b5ba66c1a18393f1200f42e8c2bb
Isaac Odeyemi <117050662+isaacodee@users.noreply.github.com>
2024-10-08 21:32:45 -0400

    Add files via upload

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,8 +1,9 @@
 function validateForm() {
-    const name = document.forms["contactForm"]["name"].value;
-    const email = document.forms["contactForm"]["email"].value;
-    if (name === "" || email === "") {
-        alert("Name and email must be filled out");
-        return false;
+    const name = document.forms["contactForm"]["name"].value; // Get the value of the "name" field from the form with the name "contactForm"
+    const email = document.forms["contactForm"]["email"].value; // Get the value of the "email" field from the form with the name "contactForm"
+    
+    if (name === "" || email === "") { // Check if either the "name" or "email" field is empty
+        alert("Name and email must be filled out"); // Display an alert message if any of the fields are empty
+        return false; // Prevent the form from being submitted
     }
 }
